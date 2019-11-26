eba7ab26849cdfe6bd9e2cd860367fec87ea66d6
harshiththota <thota.harshit@gmail.com>
2019-11-27 00:18:07 +0530

    Send result file in the response

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const formidable = require('formidable')
 const Helper = require('./helper.js')
+const csv = require('csv');
+const fileSystem = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -14,15 +17,21 @@ app.get('/', function (req, res) {
 app.post('/submit', function (req, res) {
   new formidable.IncomingForm().parse(req, (err, fields, files) => {
     if (err) {
-      console.error('Error', err)
       throw err
     }
-    console.log('fields : ', fields);
-    console.log('files : ', files);
     return Helper.getDisbursement(fields, files)
-      .then((result) => {
-        console.log('result : ', result);
-        res.send(' Submitted Successfully!');
+      .then(() => {
+        var filePath = path.join(__dirname, 'result.csv');
+        var stat = fileSystem.statSync(filePath);
+
+        res.writeHead(200, {
+          'Content-Type': 'text/csv',
+          'Content-Length': stat.size
+        });
+
+        var readStream = fileSystem.createReadStream(filePath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(res);
       })
   })
 });
