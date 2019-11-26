f038ef11933eb4e18269ccb4c14259c4ba4adee0
harshiththota <thota.harshit@gmail.com>
2019-11-26 22:10:18 +0530

    Add helper funcs file

---
+++ helper.js
@@ -0,0 +1,32 @@
+const csv = require('csvtojson');
+const _ = require('lodash');
+
+const ROW = {
+  DATE: 'Date',
+  ORDER_ID: 'Order Id',
+  NONPROFIT: 'Nonprofit',
+  DONATION_CURRENCY: 'Donation Currency',
+  DONATION_AMOUNT: 'Donation Currency',
+  FEE: 'Fee'
+};
+
+exports.getDisbursement = function (fields, files) {
+  // console.log('fields ; ', fields);
+  // console.log('files : ', files.csvFile);
+
+  return csv()
+    .fromFile(files.csvFile.path)
+    .then((result) => {
+      // console.log(result);
+      console.log('** : ', result.length);
+      
+      result = _.filter(result, (obj) => {
+        if (obj[ROW.DATE] && obj[ROW.ORDER_ID] && obj[ROW.NONPROFIT]
+          && obj[ROW.FEE] && obj[ROW.DONATION_CURRENCY] && obj[ROW.DONATION_AMOUNT]) {
+            return obj;
+          }
+      });
+
+      console.log('result ; ', result.length);
+    })
+};
